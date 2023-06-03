c5d2385ba543774062047e9ee37f515ce97c4bb7
psylenda <65675751+psylenda@users.noreply.github.com>
2023-06-02 23:48:26 -0700

    let's go

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    returnVal = ""
+    if (num == 0) {
+        return returnVal
+    }
+    else if (num == 1) {
+        return str
+    }
+    else if (num < 0) {
+        return "ERROR"
+    }
+    for (let i = 0; i < num; i++) {
+        returnVal += str
+    }
+    return returnVal
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    returnVal = [...input].reverse().join('')
+    return returnVal
 };
 
 // Do not edit below this line
