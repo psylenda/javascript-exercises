e3ec4435ecc1cd7585cdaefa370b998157da37c4
psylenda <hongjonsilver@gmail.com>
2023-06-07 08:48:39 -0700

    added all exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
 
+const fibonacci = function(input) {
+    if (input == 1) {
+        return 1;
+    }
+    else if (input == 0) {
+        return 0;
+    }
+    return fibonacci(input-2) + fibonacci(input-1)
 };
 
 // Do not edit below this line
