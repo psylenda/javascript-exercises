09a7126ec24dd46c34a39d0ea2f941bbadce1ab5
psylenda <hongjonsilver@gmail.com>
2023-06-07 07:32:58 -0700

    exercises 4 and 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)){
+            newArray.push(item)
+        }
+    });
 
-};
+    return newArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    finalSum = first+second
+    if (!Number.isInteger(first) || !Number.isInteger(second) || first < 0 || second < 0) {
+        return 'ERROR'
+    }
+    for (let i = Math.min(first,second)+1; i < Math.max(first,second); i++){
+        finalSum += i
+    }
 
+    return finalSum
 };
 
 // Do not edit below this line
