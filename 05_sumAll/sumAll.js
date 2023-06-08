f3e33784e52eb77a4a1b1e54058118b4efbda5fb
psylenda <hongjonsilver@gmail.com>
2023-06-08 14:13:42 -0700

    changed exercises

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(first,second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)|| first < 0 || second < 0){
+        return 'ERROR'
+    }
+    var returnValue = first+second;
+    for (let i = Math.min(first,second)+1; i < Math.max(first,second); i++){
+        returnValue += i;
+    } 
+    return returnValue
 };
 
 // Do not edit below this line
